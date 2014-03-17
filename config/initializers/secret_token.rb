df9ab94ea3d5edfaf207c21098e75423e34e4b3a
anuj-dhanju <anujdhanju@gmail.com>
2014-03-17 13:19:38 +0530

    Spree Installation
    Imagemagick installation on window and configure spree application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = '361c1992c6d61c410a3cdc5d1a9cc6f6df489bb246faf60ca20bbd9823705dc2f1ad175f042127bd08c50a1adf479cabe7d3f50b258c818ec8ca89c46b6ef434'
